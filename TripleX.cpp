1b56d1d85655cc257e18775d8555ecdbbaa3b6a1
Henry-Ning <henryhuning@gmail.com>
2020-06-29 12:21:02 -0700

    18. Hello, World! 9min

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,1 +1,9 @@
-int main(){return 0;}
+#include <iostream>
+
+int main()
+{
+    std::cout << "You are a secret agent breaking into a secure server room...";
+    std::cout << std::endl;
+    std::cout << "You need to enter the correct codes to continue...";
+    return 0;
+}
